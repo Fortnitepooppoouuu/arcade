2c59519815c700b6e3c5b1cf5f0aceb76a9bd559
FinleyA08 <fin.austin08@gmail.com>
2024-11-30 11:32:00 -0600

    added tap tap shots

--- go.js
+++ go.js
@@ -1,3 +1,7 @@
 function space() {
   window.location.href = "https://www.crazygames.com/game/space-waves";
 }
+
+function taptap() {
+  window.location.href = "https://www.crazygames.com/game/tap-tap-shots";
+}
